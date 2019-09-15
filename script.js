19aa39de2ca784308e8b09680ee566305675282e
Carlos Eduardo Pereira Marques <ra214327@paraguai.lab.ic.unicamp.br>
2019-09-15 19:38:56 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openMenu() {
+  document.getElementById("side-menu").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeMenu() {
+  document.getElementById("side-menu").style.width = "0";
+}
