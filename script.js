f6cf4ccd7d36fd4ed3ba76881392f4195ef44adf
Carlos Eduardo Pereira Marques <ra214327@fernandes.LABS-IC>
2019-10-23 15:44:09 -0300

    Initial upload

---
+++ script.js
@@ -0,0 +1,60 @@
+function openMenu() {
+  document.getElementById("side-menu").style.width = "250px";
+}
+
+function closeMenu() {
+  document.getElementById("side-menu").style.width = "0";
+}
+
+$(window).scroll(function () {
+  var myElem = document.getElementById('intro');
+  if (myElem === null) return;
+
+
+  var scroll = $(window).scrollTop();
+
+  if (scroll <= 10) {
+    $("#header").addClass("transparent");
+    $("#side-menu").addClass("links-height-fix");
+    $("#closebtn").addClass("cross-height-fix");
+  } else {
+    $("#header").removeClass("transparent");
+    $("#side-menu").removeClass("links-height-fix");
+    $("#side-menu #closebtn").removeClass("cross-height-fix");
+  }
+});
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top-vh(9)+1
+      }, 800, function(){});
+      closeMenu();
+    }
+  });
+});
+
+function vh(v) {
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  return (v * h) / 100;
+}
+
+function parallax() {
+	var $slider = document.getElementById("slider");
+
+	var yPos = window.pageYOffset / $slider.dataset.speed;
+	yPos = -yPos;
+
+	var coords = '40% '+ yPos + 'px';
+
+	$slider.style.backgroundPosition = coords;
+}
+
+window.addEventListener("scroll", function(){
+	parallax();
+});
